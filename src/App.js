46cf187a2c562a0422713a6d2d478182f549aac8
Akhibgithub <stark010402@gmail.com>
2022-12-30 01:11:55 +0530

    Setup Done.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Portfolio from "./Components/Portfolio/Portfolio";
+
+function App() {
+  return (
+    <div>   
+      <Portfolio/>
+    </div>
+  );
+}
+
+export default App;

d788445360c6b4f50cb04110209fd760ff816d0d
Akhibgithub <stark010402@gmail.com>
2022-12-31 14:07:26 +0530

    Subcontainer updated.

--- src/Components/Portfolio/SubContainer.jsx
+++ src/Components/Portfolio/SubContainer.jsx
@@ -8,7 +8,7 @@ const SubContainer = () => {
       <h2 className="Big-heading">Shaikh Aaqib.</h2>
       <h3 className="eEjAsd">I build things for the web.</h3>
       <p className="eEjAsb">
-        I’m a software engineer specializing in building (and occasionally
+        I’m a software developer specializing in building (and occasionally
         designing) exceptional digital experiences. Currently, I’m focused on
         learning new technologies.
       </p>

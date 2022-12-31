99a301d0a90333797ed14c24fe76f2dbffac3a6a
Akhibgithub <stark010402@gmail.com>
2022-12-31 14:09:38 +0530

    WorkTwo comp updated.

--- src/Components/Portfolio/WorkProjTwo.jsx
+++ src/Components/Portfolio/WorkProjTwo.jsx
@@ -1,38 +1,44 @@
 import React from "react";
-import ReactCrud from './ReactCrud.jpg'
+import ReactCrud from "./ReactCrud.jpg";
 
 const WorkProjTwo = () => {
   return (
     <div className="work">
-            <div className="work-sub-container">
-      <div className="projects">
-        <h6>Featured Project</h6>
-        <a href="#">React Crud Application</a>
-      </div>
-      <div className="caldesc">
-        <p>
-          React Crud Application with the help of JSON. Performed CRUD
-          operations using React and React Hooks! all operations like Get, Put,
-          Post and Delete request with the help of json server.
-        </p>
-      </div>
-      <div>
-        <div className="Img-border"></div>
-        <div className="project-cal-Img">
-          <a href="">
-            <img src={ReactCrud} alt="ReactCrudApplication Image" width="500px" height="300px" />
-          </a>
+      <div className="work-sub-container">
+        <div>
+          <div className="projects">
+            <h6>Featured Project</h6>
+            <a href="">React Crud Application</a>
+          </div>
+          <div className="caldesc">
+            <p>
+              React Crud Application with the help of JSON. Performed CRUD
+              operations using React and React Hooks! all operations like Get,
+              Put, Post and Delete request with the help of json server.
+            </p>
+          </div>
+          <div></div>
+          <div className="cal-tech">
+            <ul>
+              <li>JavaScript (ES6+)</li>
+              <li>Reactjs</li>
+              <li>Bootstrap</li>
+            </ul>
+            <ul>
+              <li>Material UI</li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="cal-tech">
-        <ul>
-          <li>JavaScript (ES6+),</li>
-          <li>Reactjs,</li>
-          <li>Bootstrap,</li>
-          <br />
-          <li>Material UI</li>
-        </ul>
-      </div>
+        <div>
+          <div className="project-img-container">
+            <div className="Img-border"></div>
+            <div className="project-cal-Img">
+              <a href="">
+                <img src={ReactCrud} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
